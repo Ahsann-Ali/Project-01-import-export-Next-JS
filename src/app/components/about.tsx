a3dad5ae03cba84d375bf8fe69c4decd0993a59d
Ahsann-Ali <saltyswag372@gmail.com>
2024-09-21 19:04:19 +0500

    first commit

---
+++ src/app/components/about.tsx
@@ -0,0 +1,6 @@
+export default function About(){
+    return <>
+    <h2><b><u>About</u></b></h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, eaque aspernatur dolorum magnam fuga exercitationem non voluptatibus ratione dignissimos optio!</p>
+    </>
+}
---
+++ src/app/components/hearder.tsx
@@ -0,0 +1,6 @@
+export default function Header(){
+    return <>
+    <h2><b><u>Header</u></b></h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </>
+}
